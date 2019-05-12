4092441746f55e821d5a41fcd0fcb9e8ecfce92f
Lufec <30414428+Lufec@users.noreply.github.com>
2019-05-12 14:13:37 -0300

    Include main implementation

--- projeto2/main.cpp
+++ projeto2/main.cpp
@@ -49,12 +49,66 @@ int main()
     vector<FiguraGeometrica*> figs;
 
 
-    figs.push_back(new PutBox(10,14,10,14,10,14,1,1,0,1));
+    for(int i=1; i<linhas.size();i++){
+        stringstream tipo(linhas[i]);
+        tipo >> nome;
 
-    figs[0]->draw(t);
+        if( nome == "putVoxel"){
+            int x,y,z;
+            float r,g,b,a;
+            tipo >> x >> y >> z >> r>> g >> b >> a;
+            figs.push_back(new PutVoxel(x,y,z,r,g,b,a));
+        }
 
-    delete figs[0];
+        else if (nome == "cutVoxel"){
+            int x,y,z;
+            tipo >>x >> y >> z;
+            figs.push_back(new CutVoxel(x,y,z));
+        }
+        else if( nome == "putBox"){
+            int x0,x1,y0,y1,z0,z1;
+            float r,g,b,a;
+            tipo >> x0 >> x1 >> y0 >> y1 >> z0 >> z1 >> r >> g >> b >> a;
+            figs.push_back(new PutBox(x0,x1,y0,y1,z0,z1,r,g,b,a));
+        }
+        else if( nome == "cutBox"){
+            int x0,x1,y0,y1,z0,z1;
+            tipo >> x0 >> x1 >> y0 >> y1 >> z0 >> z1;
+            figs.push_back(new CutBox(x0,x1,y0,y1,z0,z1));
+        }
+        else if(nome == "putSphere"){
+            int x,y,z,rd;
+            float r,g,b,a;
+            tipo >> x >> y >> z >> rd >> r >> g >>b >>a;
+            figs.push_back(new PutSphere(x,y,z,rd,r,g,b,a));
 
+        }
+
+        else if(nome == "cutSphere"){
+            int x,y,z,rd;
+            tipo >> x >> y >> z >> rd;
+            figs.push_back(new CutSphere(x,y,z,rd));
+        }
+
+        else if (nome == "putEllipsoid") {
+            int x,y,z,rx,ry,rz;
+            float r,g,b,a;
+            tipo >> x >> y >> z >> rx >> ry >> rz >> r >> g >> b >> a;
+            figs.push_back(new PutEllipsoid(x,y,z,rx,ry,rz,r,g,b,a));
+        }
+        else if (nome == "cutEllipsoid") {
+            int x,y,z,rx,ry,rz;
+            tipo >> x >> y >> z >> rx >> ry >> rz;
+            figs.push_back(new CutEllipsoid(x,y,z,rx,ry,rz));
+        }
+    }
+
+    for (int i =0; i<figs.size(); i++){
+        figs[i]->draw(t);
+    }
+    for (int i =0; i<figs.size(); i++){
+        delete figs[i];
+    }
 
     t.writeOFF("/home/lufec/Desktop/Prog Avanc/Parte2/projeto2/off.off");
     t.writeVECT("/home/lufec/Desktop/Prog Avanc/Parte2/projeto2/vect.vect");
