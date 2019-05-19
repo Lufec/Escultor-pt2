3a1ebc85279ba36b3bc344301b6288f4f5dc7407
Lufec <30414428+Lufec@users.noreply.github.com>
2019-05-19 13:32:29 -0300

    Deleting func from scp header

--- projeto2/sculptor.h
+++ projeto2/sculptor.h
@@ -21,12 +21,6 @@ public:
   void setColor(float _r, float _g, float _b, float alpha);
   void putVoxel(int x, int y, int z);
   void cutVoxel(int x, int y, int z);
-  void putBox(int x0, int x1, int y0, int y1, int z0, int z1);
-  void cutBox(int x0, int x1, int y0, int y1, int z0, int z1);
-  void putSphere(int xcenter, int ycenter, int zcenter, int radius);
-  void cutSphere(int xcenter, int ycenter, int zcenter, int radius);
-  void putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
-  void cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
   void writeOFF(const std::string filename);
   void writeVECT(const std::string filename);
   ~Sculptor();
